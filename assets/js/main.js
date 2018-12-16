49aff30930464995cd649bd6fc3ceb962be8a69b
alexAlice <alex@alice.si>
2018-12-16 03:48:18 +0100

    added particular-js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,11 @@ function showWhoWeAreAnswer() {
 function showWhatWeVeDoneAnswer() {
     alert('We have created a lot of ideas. They all are so good so we can\'t choose the best one');
 }
+
+function showPrivacyPolicy() {
+    alert('We don\'t use your cookies yet');
+}
+
+function onInvestClick() {
+    alert('Sorry, we had not time to implement it yet. But thank you for your generosity')
+}
